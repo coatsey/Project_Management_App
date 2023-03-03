4f9392ff48b65952480dca4fe16eafea83dfe8cd
Logan <coates.logan1125@gmail.com>
2023-03-03 10:40:54 -0700

    added IntroActivity

--- app/src/main/java/com/example/projectmanagement/IntroActivity.kt
+++ app/src/main/java/com/example/projectmanagement/IntroActivity.kt
@@ -1,25 +1,17 @@
 package com.example.projectmanagement
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.WindowManager.*
 
-
-class SplashActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_intro)
 
         window.setFlags(
             LayoutParams.FLAG_FULLSCREEN,
             LayoutParams.FLAG_FULLSCREEN
         )
-
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 2500)
     }
 }
