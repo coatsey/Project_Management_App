2ffb2936783561146fa732d1f0e508755c8c429b
Logan <coates.logan1125@gmail.com>
2023-03-06 14:19:47 -0700

    added signUp activity

--- app/src/main/java/com/example/projectmanagement/IntroActivity.kt
+++ app/src/main/java/com/example/projectmanagement/IntroActivity.kt
@@ -1,8 +1,11 @@
 package com.example.projectmanagement
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager.*
+import kotlinx.android.synthetic.main.activity_intro.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +16,9 @@ class IntroActivity : AppCompatActivity() {
             LayoutParams.FLAG_FULLSCREEN,
             LayoutParams.FLAG_FULLSCREEN
         )
+
+        btn_sign_up_intro.setOnClickListener{
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
