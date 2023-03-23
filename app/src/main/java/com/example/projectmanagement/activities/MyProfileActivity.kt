34643f8501b5e9dbffbbd026bd28a306e55beb4a
Logan <coates.logan1125@gmail.com>
2023-03-23 15:55:45 -0600

    added Boards

--- app/src/main/java/com/example/projectmanagement/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/projectmanagement/activities/MyProfileActivity.kt
@@ -25,11 +25,6 @@ import java.io.IOException
 
 class MyProfileActivity : BaseActivity() {
 
-    companion object {
-        private const val READ_STORAGE_PERMISSION_CODE = 1
-        private const val PICK_IMAGE_REQUEST_CODE = 2
-    }
-
     private var mSelectedImageFileUri: Uri? = null
     private lateinit var mUserDetails: User
     private var mProfileImageURL : String = ""
@@ -42,12 +37,12 @@ class MyProfileActivity : BaseActivity() {
         iv_profile_user_image.setOnClickListener{
             if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
             == PackageManager.PERMISSION_GRANTED){
-                showImageChooser()
+                Constants.showImageChooser(this)
             }else{
                 ActivityCompat.requestPermissions(
                     this,
                     arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
-                    READ_STORAGE_PERMISSION_CODE
+                    Constants.READ_STORAGE_PERMISSION_CODE
                 )
             }
         }
@@ -67,9 +62,9 @@ class MyProfileActivity : BaseActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                showImageChooser()
+                Constants.showImageChooser(this)
             } else {
                 Toast.makeText(
                     this,
@@ -80,18 +75,11 @@ class MyProfileActivity : BaseActivity() {
         }
     }
 
-    private fun showImageChooser() {
-        val galleryIntent = Intent(
-            Intent.ACTION_PICK,
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        )
-        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
-    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK
-            && requestCode == PICK_IMAGE_REQUEST_CODE
+            && requestCode == Constants.PICK_IMAGE_REQUEST_CODE
             && data!!.data != null
         ) {
             mSelectedImageFileUri = data.data
@@ -164,7 +152,7 @@ class MyProfileActivity : BaseActivity() {
             val sRef : StorageReference =
                 FirebaseStorage.getInstance().reference.child("USER_IMAGE" +
                         System.currentTimeMillis() + "." +
-                        getFileExtension(mSelectedImageFileUri))
+                        Constants.getFileExtension(this, mSelectedImageFileUri!!))
 
             sRef.putFile(mSelectedImageFileUri!!).addOnSuccessListener {
                 taskSnapshot ->
@@ -190,11 +178,6 @@ class MyProfileActivity : BaseActivity() {
                 hideProgressDialog()
             }
         }
-    }
-
-    private fun getFileExtension(uri: Uri?): String?{
-        return MimeTypeMap.getSingleton().getExtensionFromMimeType(
-            contentResolver.getType(uri!!))
     }
 
     fun profileUpdateSuccess(){
--- app/src/main/java/com/example/projectmanagement/firebase/FirestoreClass.kt
+++ app/src/main/java/com/example/projectmanagement/firebase/FirestoreClass.kt
@@ -3,10 +3,8 @@ package com.example.projectmanagement.firebase
 import android.app.Activity
 import android.util.Log
 import android.widget.Toast
-import com.example.projectmanagement.activities.MainActivity
-import com.example.projectmanagement.activities.MyProfileActivity
-import com.example.projectmanagement.activities.SignInActivity
-import com.example.projectmanagement.activities.SignUpActivity
+import com.example.projectmanagement.activities.*
+import com.example.projectmanagement.models.Board
 import com.example.projectmanagement.models.User
 import com.example.projectmanagement.utils.Constants
 import com.google.firebase.auth.FirebaseAuth
@@ -15,61 +13,187 @@ import com.google.firebase.firestore.SetOptions
 
 class FirestoreClass {
 
+    // Create a instance of Firebase Firestore
     private val mFireStore = FirebaseFirestore.getInstance()
 
-    fun registerUser(activity: SignUpActivity, userInfo : User){
+    /**
+     * A function to make an entry of the registered user in the firestore database.
+     */
+    fun registerUser(activity: SignUpActivity, userInfo: User) {
+
         mFireStore.collection(Constants.Users)
-            .document(getCurrentUserId())
+            // Document ID for users fields. Here the document it is the User ID.
+            .document(getCurrentUserID())
+            // Here the userInfo are Field and the SetOption is set to merge. It is for if we wants to merge
             .set(userInfo, SetOptions.merge())
             .addOnSuccessListener {
+
+                // Here call a function of base activity for transferring the result to it.
                 activity.userRegisteredSuccess()
             }
-    }
-
-    fun updateUserProfileData(activity: MyProfileActivity,
-                              userHashMap: HashMap<String, Any>){
-        mFireStore.collection(Constants.Users)
-            .document(getCurrentUserId())
-            .update(userHashMap)
-            .addOnSuccessListener {
-                Log.i(activity.javaClass.simpleName, "profile updated")
-                Toast.makeText(activity, "profile has been updated!",Toast.LENGTH_LONG).show()
-                activity.profileUpdateSuccess()
-            }.addOnFailureListener {
-                e ->
+            .addOnFailureListener { e ->
                 activity.hideProgressDialog()
-                Log.e(activity.javaClass.simpleName,
-                    "profile updated")
-                Toast.makeText(activity, "profile failed to updated!",Toast.LENGTH_LONG).show()
+                Log.e(
+                    activity.javaClass.simpleName,
+                    "Error writing document",
+                    e
+                )
             }
     }
 
-    fun loadUserData(activity: Activity){
+    // TODO (Step 5: Add a parameter to check whether to read the boards list or not.)
+    /**
+     * A function to SignIn using firebase and get the user details from Firestore Database.
+     */
+    fun loadUserData(activity: Activity, isToReadBoardsList: Boolean = false) {
+
+        // Here we pass the collection name from which we wants the data.
         mFireStore.collection(Constants.Users)
-            .document(getCurrentUserId())
+            // The document id to get the Fields of user.
+            .document(getCurrentUserID())
             .get()
             .addOnSuccessListener { document ->
-                val loggedInUser = document.toObject(User::class.java)
-                when(activity){
-                    is SignInActivity ->{
-                        activity.signInSuccess(loggedInUser!!)
+                Log.e(activity.javaClass.simpleName, document.toString())
+
+                // Here we have received the document snapshot which is converted into the User Data model object.
+                val loggedInUser = document.toObject(User::class.java)!!
+
+                // Here call a function of base activity for transferring the result to it.
+                when (activity) {
+                    is SignInActivity -> {
+                        activity.signInSuccess(loggedInUser)
                     }
-                    is MainActivity ->{
-                        activity.updateNavigationUserDetails(loggedInUser!!)
+                    is MainActivity -> {
+                        activity.updateNavigationUserDetails(loggedInUser, isToReadBoardsList)
                     }
-                    is MyProfileActivity ->{
-                        activity.setUserDataInUI(loggedInUser!!)
+                    is MyProfileActivity -> {
+                        activity.setUserDataInUI(loggedInUser)
                     }
                 }
             }
+            .addOnFailureListener { e ->
+                // Here call a function of base activity for transferring the result to it.
+                when (activity) {
+                    is SignInActivity -> {
+                        activity.hideProgressDialog()
+                    }
+                    is MainActivity -> {
+                        activity.hideProgressDialog()
+                    }
+                    is MyProfileActivity -> {
+                        activity.hideProgressDialog()
+                    }
+                }
+                Log.e(
+                    activity.javaClass.simpleName,
+                    "Error while getting loggedIn user details",
+                    e
+                )
+            }
+    }
+
+    /**
+     * A function to update the user profile data into the database.
+     */
+    fun updateUserProfileData(activity: MyProfileActivity, userHashMap: HashMap<String, Any>) {
+        mFireStore.collection(Constants.Users) // Collection Name
+            .document(getCurrentUserID()) // Document ID
+            .update(userHashMap) // A hashmap of fields which are to be updated.
+            .addOnSuccessListener {
+                // Profile data is updated successfully.
+                Log.e(activity.javaClass.simpleName, "Profile Data updated successfully!")
+
+                Toast.makeText(activity, "Profile updated successfully!", Toast.LENGTH_SHORT).show()
+
+                // Notify the success result.
+                activity.profileUpdateSuccess()
+            }
+            .addOnFailureListener { e ->
+                activity.hideProgressDialog()
+                Log.e(
+                    activity.javaClass.simpleName,
+                    "Error while creating a board.",
+                    e
+                )
+            }
+    }
+
+    /**
+     * A function for creating a board and making an entry in the database.
+     */
+    fun createBoard(activity: CreateBoardActivity, board: Board) {
+
+        mFireStore.collection(Constants.BOARDS)
+            .document()
+            .set(board, SetOptions.merge())
+            .addOnSuccessListener {
+                Log.e(activity.javaClass.simpleName, "Board created successfully.")
+
+                Toast.makeText(activity, "Board created successfully.", Toast.LENGTH_SHORT).show()
+
+                activity.boardCreatedSuccessfully()
+            }
+            .addOnFailureListener { e ->
+                activity.hideProgressDialog()
+                Log.e(
+                    activity.javaClass.simpleName,
+                    "Error while creating a board.",
+                    e
+                )
+            }
+    }
+
+    // TODO (Step 4: Create a function to get the list of created boards from the database.)
+    // START
+    /**
+     * A function to get the list of created boards from the database.
+     */
+    fun getBoardsList(activity: MainActivity) {
+
+        // The collection name for BOARDS
+        mFireStore.collection(Constants.BOARDS)
+            // A where array query as we want the list of the board in which the user is assigned. So here you can pass the current user id.
+            .whereArrayContains(Constants.ASSIGNED_TO, getCurrentUserID())
+            .get() // Will get the documents snapshots.
+            .addOnSuccessListener { document ->
+                // Here we get the list of boards in the form of documents.
+                Log.e(activity.javaClass.simpleName, document.documents.toString())
+                // Here we have created a new instance for Boards ArrayList.
+                val boardsList: ArrayList<Board> = ArrayList()
+
+                // A for loop as per the list of documents to convert them into Boards ArrayList.
+                for (i in document.documents) {
+
+                    val board = i.toObject(Board::class.java)!!
+                    board.documentId = i.id
+
+                    boardsList.add(board)
+                }
+
+                // Here pass the result to the base activity.
+                activity.populateBoardsListToUI(boardsList)
+            }
+            .addOnFailureListener { e ->
+
+                activity.hideProgressDialog()
+                Log.e(activity.javaClass.simpleName, "Error while creating a board.", e)
+            }
+    }
+    // END
+
+    /**
+     * A function for getting the user id of current logged user.
+     */
+    fun getCurrentUserID(): String {
+        // An Instance of currentUser using FirebaseAuth
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+        // A variable to assign the currentUserId if it is not null or else it will be blank.
+        var currentUserID = ""
+        if (currentUser != null) {
+            currentUserID = currentUser.uid
         }
 
-   fun getCurrentUserId(): String{
-       var currentUser = FirebaseAuth.getInstance().currentUser
-       var currentUserID = ""
-       if (currentUser != null){
-           currentUserID = currentUser.uid
-       }
-       return currentUserID
+        return currentUserID
     }
 }
