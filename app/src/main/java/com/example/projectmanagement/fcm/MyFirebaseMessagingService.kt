f92c9417f1e17ddfd817433beb0ddcd57e7577b1
Logan <coates.logan1125@gmail.com>
2023-04-17 12:35:43 -0600

    added notification message for being added to board

--- app/src/main/java/com/example/projectmanagement/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/projectmanagement/fcm/MyFirebaseMessagingService.kt
@@ -1,72 +1,94 @@
 package com.example.projectmanagement.fcm
 
-import android.app.Notification
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.media.Ringtone
+import android.content.SharedPreferences
 import android.media.RingtoneManager
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.example.projectmanagement.R
 import com.example.projectmanagement.activities.MainActivity
+import com.example.projectmanagement.activities.SignInActivity
+import com.example.projectmanagement.firebase.FirestoreClass
+import com.example.projectmanagement.utils.Constants
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        super.onMessageReceived(remoteMessage)
-
-        Log.d(TAG, "FROM: ${remoteMessage.from}")
+        Log.d(TAG, "From: ${remoteMessage.from}")
 
         remoteMessage.data.isNotEmpty().let {
-            Log.d(TAG, "Message data Payload: ${remoteMessage.data}")
+            Log.i(TAG, "Message data payload: " + remoteMessage.data)
+
+            val title = remoteMessage.data[Constants.FCM_KEY_TITLE]!!
+            val message = remoteMessage.data[Constants.FCM_KEY_MESSAGE]!!
+
+            sendNotification(title, message)
         }
+
         remoteMessage.notification?.let {
             Log.d(TAG, "Message Notification Body: ${it.body}")
         }
     }
-
     override fun onNewToken(token: String) {
-        super.onNewToken(token)
         Log.e(TAG, "Refreshed token: $token")
         sendRegistrationToServer(token)
     }
 
-    private fun sendRegistrationToServer(token: String?){
-
+    private fun sendRegistrationToServer(token: String?) {
+        val sharedPreferences =
+            this.getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString(Constants.FCM_TOKEN, token)
+        editor.apply()
     }
 
-    private fun sendNotification(messageBody: String){
-        val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(this,
-        0, intent, PendingIntent.FLAG_ONE_SHOT)
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun sendNotification(title: String, message: String) {
+
+        val intent: Intent = if (FirestoreClass().getCurrentUserID().isNotEmpty()) {
+            Intent(this, MainActivity::class.java)
+        } else {
+            Intent(this, SignInActivity::class.java)
+        }
+        intent.flags = (Intent.FLAG_ACTIVITY_NEW_TASK
+                or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
         val channelId = this.resources.getString(R.string.default_notification_channel_id)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.ic_stat_ic_notification)
-            .setContentTitle("Title")
-            .setContentText("Message")
+            .setContentTitle(title)
+            .setContentText(message)
             .setAutoCancel(true)
             .setSound(defaultSoundUri)
             .setContentIntent(pendingIntent)
 
-        val notificationManager = getSystemService(
-            Context.NOTIFICATION_SERVICE
-        ) as NotificationManager
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            val channel = NotificationChannel(channelId,
-            "Channel Project Management Title",
-            NotificationManager.IMPORTANCE_DEFAULT)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel Projemanag title",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
             notificationManager.createNotificationChannel(channel)
         }
-        notificationManager.notify(0, notificationBuilder.build())
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
     }
 
     companion object {
